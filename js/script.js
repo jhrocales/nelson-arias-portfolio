f0b1c988410534da35a810928621dd430cf6929e
jhrocales <jhrocales@gmail.com>
2025-12-16 03:26:01 +0800

    First commit: Client Nelson Arias

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// typing animation
+
+var typed = new Typed(".typing", {
+  strings: [
+    "",
+    "Graphics Design",
+    "Encoding",
+    "Copywriting",
+    "Blogging",
+    "Scheduling",
+  ],
+  typeSpeed: 80,
+  BackSpeed: 60,
+  loop: true,
+});
